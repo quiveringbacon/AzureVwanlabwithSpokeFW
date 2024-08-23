8440033eaebc7094e563bc258ca0cfdc8c5b8b42
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-23 12:51:51 -0500

    Update main.tf
    changed ip allocation

--- main.tf
+++ main.tf
@@ -382,7 +382,7 @@ resource "azurerm_public_ip" "spoke1vm-pip" {
   name                = "spoke1vm-pip"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   timeouts {
     create = "2h"
     read = "2h"
@@ -395,7 +395,7 @@ resource "azurerm_public_ip" "spoke2vm-pip" {
   name                = "spoke2vm-pip"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   timeouts {
     create = "2h"
     read = "2h"
